f1e238118dedb4d7da65ea5acf9a3ba6704a452a
Benjamin Samuel <b.k.samuel412@gmail.com>
2023-10-20 18:46:21 -0500

    Updated lab 8 files

--- Lab8/lab8.js
+++ Lab8/lab8.js
@@ -1,4 +1,15 @@
 
 function getDataFromForm() {
-  alert("it worked!");
+  $names = $("[name]")
+  //console.log($names.data().text)
+  runAjax($names.data("fname").text, $names.data("lname").text)
+}
+
+function runAjax(fname, lname){
+  //var xhr = new XMLHttpRequest
+  //xhr.open('GET', './ajax.php', true)
+  //xhr.send(`fname=${fname}, lname=${lname}`)
+  $.get('./ajax.php', {"fname": fname, "lname": lname}, function(){
+    $("#responseString").text("Request succeeded")
+  }, text)
 }
