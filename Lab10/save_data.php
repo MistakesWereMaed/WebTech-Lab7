cb7d8763aafb777edd41741722250570e274325f
Benjamin Samuel <b.k.samuel412@gmail.com>
2023-11-05 15:25:14 -0600

    Updated lab 10

--- Lab10/save_data.php
+++ Lab10/save_data.php
@@ -1,7 +1,7 @@
 <?php
 
-$file = fopen("data.txt", "a");
-$txt = $_POST['fname'] + ' ' + $_POST['lname'];
+$file = fopen("data.txt", "a+");
+$txt = $_POST['fname'] .''. ' ' .''. $_POST['lname'] .''. "\n";
 fwrite($file, $txt);
 fclose($file);
 
--- Lab10/show_data.php
+++ Lab10/show_data.php
@@ -4,5 +4,6 @@ $file = fopen("data.txt", "r");
 while(!feof($file)){
     $line = fgets($file);
     echo $line;
+    echo "<br />";
 }
 fclose($file);
