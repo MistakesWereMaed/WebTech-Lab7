558fb71aa846c0b5e2c4d304bd21625f737b3adb
Benjamin Samuel <b.k.samuel412@gmail.com>
2023-11-11 20:22:46 -0600

    Updated lab12

--- Lab12/index.php
+++ Lab12/index.php
@@ -1,7 +1,12 @@
 <?php
     $cookie = $_COOKIE['cs4413'] ?? 0;
-    $cookie = $cookie + 10;
-    setcookie('cs4413', $cookie, 2);
-    $message = 'Cookie value: ' . $cookie;
+    if($cookie != 0){
+        $message = 'Cookie value already set: ' . $cookie;
+    } else {
+        $cookie = 10;
+        setcookie('cs4413', $cookie, 2);
+        $message = 'New cookie value set: ' . $cookie;
+    }
+    
     echo $message
 ?>
